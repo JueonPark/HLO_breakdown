fa35ff61f3482a1d64880a585a5f7da0640ed757
jueonpark <jueonpark@postech.ac.kr>
2022-02-17 23:07:27 +0900

    made function

--- main.py
+++ main.py
@@ -4,43 +4,15 @@ hlo graph에 있는 각 fusion 별로 어떠한 computation인지 알려준다.
 import csv
 import argparse
 from hlo_parser import HloTable
-from bert_metadata import parse_metadata
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('--hlo', type=str, required=True, help="xla hlo graph path (text)")
+  parser.add_argument('--csv', type=str, required=True, help="result csv file (.csv)")
+  parser.add_argument('--hlo', type=str, required=True, help="xla hlo graph path (.txt)")
   args = parser.parse_args()
+  result_file = open(args.csv, "r")
   hlo_file = open(args.hlo, "r")
   hlo_table = HloTable(hlo_file.read()).hlo_table
-  hlo_breakdown = []
 
-  # for each fusion:
-  for hlo_fusion in hlo_table:
-    # for each fusion's metadata:
-    metadata_list = []
-    for metadata in hlo_table[hlo_fusion]["data"]:
-      parsed_metadata = parse_metadata(metadata)
-      if parsed_metadata in metadata_list:
-        continue
-      elif parsed_metadata is None:
-        continue
-      else:
-        metadata_list.append(parse_metadata(metadata))
-    # now, with metadata_list, append hlo_fusion's computation property into hlo_breakdown
-    output_name = hlo_fusion
-    output_name = "Fusion"
-    if len(metadata_list) != 0:
-      output_name += "("
-    for metadata in metadata_list:
-      output_name += metadata
-      if metadata != metadata_list[-1]:
-        output_name += "+"
-    if len(metadata_list) != 0:
-      output_name += ")"
-
-    hlo_breakdown.append(output_name)
+  # csv file을 읽고, kernel name을 바꿔준다.
   
-  # write result to file
-  with open(args.hlo[:-4] + ".breakdown", "w") as writefile:
-    for element in hlo_breakdown:
-      writefile.write(element + "\n")
---
+++ rewrite_fusion_kernel.py
@@ -0,0 +1,50 @@
+"""
+hlo graph에 있는 각 fusion 별로 어떠한 computation인지 알려준다.
+"""
+import csv
+import argparse
+from hlo_parser import HloTable
+from bert_metadata import parse_metadata
+
+def rewrite_fusion_kernel(hlo_table):
+  """
+  generate dictionary of original fusion kernel's name and its new name
+  """
+  hlo_breakdown = {}
+  # for each fusion:
+  for hlo_fusion in hlo_table:
+    # for each fusion's metadata:
+    metadata_list = []
+    for metadata in hlo_table[hlo_fusion]["data"]:
+      parsed_metadata = parse_metadata(metadata)
+      if parsed_metadata in metadata_list:
+        continue
+      elif parsed_metadata is None:
+        continue
+      else:
+        metadata_list.append(parse_metadata(metadata))
+    # now, with metadata_list, append hlo_fusion's computation property into hlo_breakdown
+    output_name = hlo_fusion
+    output_name = "Fusion"
+    if len(metadata_list) != 0:
+      output_name += "("
+    for metadata in metadata_list:
+      output_name += metadata
+      if metadata != metadata_list[-1]:
+        output_name += "+"
+    if len(metadata_list) != 0:
+      output_name += ")"
+    hlo_breakdown[hlo_fusion] = output_name
+  return hlo_breakdown
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--hlo', type=str, required=True, help="xla hlo graph path (text)")
+  args = parser.parse_args()
+  hlo_file = open(args.hlo, "r")
+  hlo_table = HloTable(hlo_file.read()).hlo_table
+  
+  # write result to file
+  hlo_breakdown = rewrite_fusion_kernel(hlo_table)
+  for name in hlo_breakdown:
+    print(name + ": " + hlo_breakdown[name])
